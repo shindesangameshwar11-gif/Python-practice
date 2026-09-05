80815a630392195f588802031d943155779867db
shindesangameshwar11-gif <shindesangameshwar11@gmail.com>
2026-09-05 16:43:21 +0530

    Grades Calculate Program

---
+++ Grade_cal.py
@@ -0,0 +1,12 @@
+#wap for grade calculator
+a=int(input("Enter Your marks:"))
+if(a>=90):
+    print("Grade A")
+elif(a>=75):
+    print("Grade B")
+elif(a>=50):
+    print("Grade C")
+elif(a>=40):
+    print("Grade D")
+else:
+    print("fail")
