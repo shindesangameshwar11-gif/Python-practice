8c8f253f8283900e217dfee0cc54a61a690538cc
shindesangameshwar11-gif <shindesangameshwar11@gmail.com>
2026-09-05 16:42:16 +0530

    User login program

---
+++ User_login.py
@@ -0,0 +1,9 @@
+a=1103
+b=00000
+c=int(input("Enter the Username:"))
+d=int(input("Enter the password:"))
+
+if(a==c and b==d):
+    print("Login successful")
+else:
+    print("invalid username and password")
