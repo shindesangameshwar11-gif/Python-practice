074cf6bd903328cb102a5dc4153ca6fcb91167dc
shindesangameshwar11-gif <shindesangameshwar11@gmail.com>
2026-09-05 16:33:29 +0530

    Add Largest of three Number program

---
+++ Largest_of_three.py
@@ -0,0 +1,11 @@
+#wap for largest of three numbers
+a=int(input("Enter A:"))
+b=int(input("Enter B:"))
+c=int(input("Enter C:"))
+
+if(a>=b):
+    print("A is the greatest value")
+elif(b>=c):
+    print("B is the greatest value")
+else:
+    print("C is the greatest value")
