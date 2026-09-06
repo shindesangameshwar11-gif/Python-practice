268108869f60ee8772c37c12c3246f4bc37d671e
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-06 11:58:58 +0530

    Simple Calculator program

---
+++ Simple_cal.py
@@ -0,0 +1,16 @@
+#This is the Simple Calculator Program
+a=int(input("Enter The Frist Number :"))
+b=int(input("Enter the second Number :"))
+c=0
+
+c=a+b
+print("Addition of two numbers :",c)
+
+c=a*b
+print("Multiplication of two numbers :",c)
+
+c=a/b
+print("divison of two numbers :",c)
+
+c=a-b
+print("Subtraction of two numbers :",c)
