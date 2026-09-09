97f36df86dfa7a4b7a8ccfa83e848f0f08cc5cf7
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-09 09:33:49 +0530

    Add files via upload
    This program for the logical operator

---
+++ Logical.py
@@ -0,0 +1,21 @@
+#wap program for logical operator
+
+#example of and operator
+a=10
+b=15
+
+print(a>5 and b<20)         #true statement
+print(a>15 and b<10)        #false statement
+
+#OR operator
+x=10
+y=20
+
+print(a>5 or b<10)
+print(a>15 or b<10)
+
+#not operator
+
+a=10
+print(not (a>5))
+print(not(a<5))
