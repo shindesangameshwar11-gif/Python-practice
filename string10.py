6058854c1d0e3bdbcfb5cfe59d6729b90c566bfc
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-12 10:49:34 +0530

    Add files via upload

---
+++ string10.py
@@ -0,0 +1,15 @@
+#wap to take a input as the string 
+#covert the string lowercase
+#covert the string cppercase
+#print the frist 3 letter
+#print the last 3 letter
+#print the find the position of a given character
+#count the number of occurrences of that character
+
+a=input("Enter Your Name :")
+print(a.upper())
+print(a.lower())
+print(a[0:3])
+print(a[::-2])
+print(a.find('m'))
+print(a.count(a))
