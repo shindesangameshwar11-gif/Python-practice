3ce71a9a5144efb75a630462106b727d828e1c7a
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-12 10:49:05 +0530

    Add files via upload

---
+++ string9.py
@@ -0,0 +1,3 @@
+#wap to take a sentence as a input and replace every occurrence of "python"with "programming." print the modifty sentence
+x="the python is the one of the most famous skill in problem solving  "
+print(x.replace("python","programming"))
