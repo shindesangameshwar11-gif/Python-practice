82f29bb72f841168742c41901c422770038cb483
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-13 12:19:45 +0530

    Add files via upload

---
+++ lists10.py
@@ -0,0 +1,22 @@
+#wap to perform this operations
+#add an element to the end of the list
+#insert an element at the specific position
+#remove and element from the list
+#sort the list ascending order
+#reverse the list
+
+a=['apple','banana','mango','watermelon']
+a.insert(5,'dragon fruit')
+print(a)
+
+a.insert(0,'pineapple')
+print(a)
+
+a.remove('apple')
+print(a)
+
+a.sort()
+print(a)
+
+a.reverse()
+print(a)
