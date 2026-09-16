6cc608b498b0578009574625652d16fb110d060e
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-16 10:01:43 +0530

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,21 @@
+#wap to create dictionary containing the name and marks of 5 students print all the values
+
+x={
+    "name1" : "Sangam,",
+    "mark1" : 99,
+
+    "name2" : "vicky",
+    "mark2" : 35,
+
+   "name3" : "abhijeet",
+    "mark3" : 35, 
+
+   "name4" : "jyote",
+   "mark4" : 35,
+
+   "name5" : "kunal",
+   "mark5" : 35,
+ 
+}
+
+print(x.keys())
