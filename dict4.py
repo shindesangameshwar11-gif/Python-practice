1eb35a58a5e6431e6b3e79251da5c92e67b4e2bb
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-16 10:02:44 +0530

    Add files via upload

---
+++ dict4.py
@@ -0,0 +1,20 @@
+#wap to create a dict containing name and marks 5 students , print all key value pairs
+s={
+    "name1" : "Sangam,",
+    "mark1" : 99,
+
+    "name2" : "vicky",
+    "mark2" : 35,
+
+   "name3" : "abhijeet",
+    "mark3" : 35, 
+
+   "name4" : "jyote",
+   "mark4" : 35,
+
+   "name5" : "kunal",
+   "mark5" : 35,
+ 
+}
+
+print(s.items())
