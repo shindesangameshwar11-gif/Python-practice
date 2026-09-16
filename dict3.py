070990fbe39b128ace107e5dc577f7dbcf1a13c8
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-16 10:02:02 +0530

    Add files via upload

---
+++ dict3.py
@@ -0,0 +1,20 @@
+#wap to create a dictionary containing name and marks 5 students and print the all values
+a={
+    "name1" : "Sangam,",
+    "mark1" : 99,
+
+    "name2" : "vicky",
+    "mark2" : 35,
+
+   "name3" : "abhijeet",
+    "mark3" : 35, 
+
+   "name4" : "jyote",
+   "mark4" : 35,
+
+   "name5" : "kunal",
+   "mark5" : 35,
+ 
+}
+
+print(a.values())
