f938df4a5fc34669cba4db97ca735beae8a995ef
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-16 10:03:26 +0530

    Add files via upload

---
+++ dict5.py
@@ -0,0 +1,8 @@
+#wap to create a dict containing person name age and city. use the get() access the value of city
+b={
+    "name" : "shinde sangam",
+    "age" : 20,
+    "city" : "Mumbai"
+}
+
+print(b.get("city"))
