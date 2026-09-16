c05a2918cfa203385fd851f39d8acb64e185a4b7
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-16 10:01:14 +0530

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,13 @@
+#wap to create a dictionary and print each value using accessing 
+personal={
+     "name": "Sangam shinde",
+     "age": 20,
+     "course": "BCA",
+     "job role": "Software engineer"
+
+     }
+
+print(personal["name"])
+print(personal["age"])
+print(personal["course"])
+print(personal["job role"])
