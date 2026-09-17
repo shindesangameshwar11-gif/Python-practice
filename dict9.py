14f2a33729b0c8cacff0c733a99aaea0175e213d
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-17 10:24:47 +0530

    Add files via upload

---
+++ dict9.py
@@ -0,0 +1,23 @@
+#WAP to create a dict containing the names and ages of 5 people. 
+#use keys(),values(),and items() to print all keys.
+
+a={
+   "name1" : "Sangam,",
+       "age1" : 20,
+   
+       "name2" : "vicky",
+       "age2" : 24,
+   
+      "name3" : "abhijeet",
+       "age3" : 19, 
+   
+      "name4" : "jyote",
+      "age4" : 21,
+   
+      "name5" : "kunal",
+      "age5" : 21,
+}
+
+print(a.keys())
+print(a.values())
+print(a.items())
