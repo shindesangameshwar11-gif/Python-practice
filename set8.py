d88c9af87793c475fe63bedfa0249e09c1bc3a02
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-20 17:11:58 +0530

    Add files via upload

---
+++ set8.py
@@ -0,0 +1,5 @@
+#wap for intersection
+#two set student entroll two degree and shown entrolled both courses print
+a={"Sangam=Bca","vicky=Mba"}
+b={"Sangam=Mba","vicky=Mba"}
+print(a.intersection(b))
