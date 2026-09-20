7b286427d325932ab43282609d032df536872dd5
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-20 17:12:43 +0530

    Add files via upload

---
+++ set10.py
@@ -0,0 +1,27 @@
+#wap to perform this operations
+#create 2 sets and print the both
+#add the new element in the frist set
+#remove a specific element in the second set
+#perform operation pop
+#print the union
+#print the intersection
+#clear the second set
+
+a={1,2,3,4,5,6}
+b={4,5,6,7,8,9}
+
+print(a)
+print(b)
+
+a.add(0)
+print(a)
+
+b.remove(9)
+print(b)
+
+a.pop
+print(a)
+
+print(a.union(b))
+print(a.intersection(b))
+print(b.clear())
