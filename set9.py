b6230583343ece898f071851e3c81a8937c534a2
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-20 17:12:27 +0530

    Add files via upload

---
+++ set9.py
@@ -0,0 +1,17 @@
+#wap perform this operation
+#add the element to the frist set
+#remove an element from the set
+#find the union of both sets
+#find the intersection of both sets
+
+a={1,2,3,4,5,6}
+b={6,7,8,9,10}
+
+a.add(0)
+print(a)
+
+b.remove(10)
+print(b)
+
+print(a.union(b))
+print(a.intersection(b))
