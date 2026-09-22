afcfc6a87f089f5d61a73e039526c9b22f2dec79
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-22 10:58:00 +0530

    Add files via upload

---
+++ break,c.py
@@ -0,0 +1,10 @@
+#wap to print the numbers form 1 to 10 using a while loop. stop the loop when the number beomes 6
+
+a=0
+
+while a<=10:
+    if(a==6):
+        break
+    a+=1
+
+print("End of loop :",a)
