2e95102e8432114923e978e8b02f750710779aee
Shinde Sangameshwar <shindesangameshwar11@gmail.com>
2026-09-24 21:51:20 +0530

    Add files via upload

---
+++ break,c8.py
@@ -0,0 +1,17 @@
+#wap to print numbers 1 to 50. skip numbers that are divisible by 5 and stop the loop number reached 40
+
+a=0
+
+while a<=50:
+
+    if a // 100:
+        a+=5
+        print(x)
+
+    elif a==40:
+          break
+
+    a = a + 5
+    print(a)
+
+print("End of loop")
